bc1c169f9823726f7bb1ddc048ace5de61d0ecda
tcat <trustut@gmail.com>
2016-03-27 19:47:08 +0300

    upd

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+//var imagic = require('imagemagick');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var gm = require('gm')
+  , imageMagick = gm.subClass({ imageMagick: true });
+
+router.get('/getfoto', function (req, res, next) {
+  imageMagick('./1.jpg')
+  .autoOrient()
+  .flip()
+  .stream('png', function (err, stdout) {
+  	console.log(err)
+    if (err) return next(err);
+    console.log('________________________________');
+   res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Expires", 0);
+    res.setHeader('Content-Type', 'image/png');
+    
+    stdout.pipe(res);
+    
+  });
 });
+
 
 module.exports = router;
